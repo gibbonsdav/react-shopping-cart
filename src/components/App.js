d9f7a6bc6c99654b51476566d6b8146601c9b585
David Gibbons <gibbonsdav@gmail.com>
2020-08-26 10:42:45 -0700

    checkout

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,17 @@ import React from "react"
 import { BrowserRouter as Router, Route } from "react-router-dom"
 import Home from "./Home"
 import Cart from "./Cart"
+import Checkout from "./Checkout"
 import { useItems } from "../redux/ducks/example/index"
 import "../styles/App.css"
 
-export default function() {
+export default function () {
   return (
     <Router>
       <div>
         <Route exact path="/" component={Home} />
         <Route path="/" component={Cart} />
+        <Route path="/checkout" component={Checkout} />
       </div>
     </Router>
   )
--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,8 +1,7 @@
-import React, { useEffect, useState } from "react"
-import { Link } from "react-router-dom"
-import { FiShoppingCart } from "react-icons/fi"
+import React from "react"
+import ReactDOM from "react-dom"
+import Cart from "./Cart"
 import { useCart } from "../redux/ducks/cart"
-import "../styles/cart/cart.css"
 
 export default (props) => {
   const { cart, isOpen, toggle, remove, add } = useCart()
@@ -12,15 +11,12 @@ export default (props) => {
   const cartItems = cart.length
 
   return (
-    <div className={`cartcontain ${isOpen && " open"}`} wholecart>
-      <button className="incartcart">
-        <FiShoppingCart onClick={(e) => toggle()} />
-      </button>
-      <div className="list">
+    <div>
+      <div className="">
         <p>You have {cartItems} items in your cart.</p>
         {cart.map((product) => (
-          <div className="product cart">
-            <div className="innercart">
+          <div className="">
+            <div className="">
               <img src={product.img.thumb} />
               <p>{product.title}</p>
               <p>{product.price.toFixed(2)}</p>
