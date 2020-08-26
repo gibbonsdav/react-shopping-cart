63fa2f89c329463c6c310e91466b5110447ec0ff
David Gibbons <gibbonsdav@gmail.com>
2020-08-25 18:39:22 -0700

    item quantity

--- src/redux/ducks/cart/index.js
+++ src/redux/ducks/cart/index.js
@@ -41,10 +41,27 @@ export default (state = initialState, action) => {
       }
       return { ...state, cart: [...state.cart, action.payload] }
     case REMOVE_FROM_CART:
-      return {
-        ...state,
-        cart: state.cart.filter((item) => item.id != action.payload),
+      if (
+        state.cart.find((item) => item.id === action.payload).quantity === 1
+      ) {
+        return {
+          ...state,
+          cart: state.cart.filter((item) => item.id != action.payload),
+        }
+      } else {
+        return {
+          ...state,
+          cart: state.cart.map((item) => {
+            if (item.id === action.payload) {
+              item.quantity = item.quantity - 1
+              return item
+            } else {
+              return item
+            }
+          }),
+        }
       }
+
     case TOGGLE_CART:
       if (action.hasOwnProperty("payload")) {
         return { ...state, isOpen: action.payload }
